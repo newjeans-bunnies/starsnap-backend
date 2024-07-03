e0a2ca3914cbe0e89be571d4a975a6d5e22b0fc7
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-07-03 23:58:29 +0900

    :zap: :: add EnableConfigurationProperties

--- src/main/kotlin/com/photo/server/starsnap/StarsnapApplication.kt
+++ src/main/kotlin/com/photo/server/starsnap/StarsnapApplication.kt
@@ -1,9 +1,12 @@
 package com.photo.server.starsnap
 
+import com.photo.server.starsnap.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties::class)
 class StarsnapApplication
 
 fun main(args: Array<String>) {
