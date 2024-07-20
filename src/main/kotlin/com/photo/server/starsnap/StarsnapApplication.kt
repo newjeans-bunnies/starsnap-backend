0a8077d3697842398a8b5a764a8b7d9840e020bf
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-07-21 00:37:38 +0900

    :zap: :: set redis(multi database)

--- src/main/kotlin/com/photo/server/starsnap/StarsnapApplication.kt
+++ src/main/kotlin/com/photo/server/starsnap/StarsnapApplication.kt
@@ -4,8 +4,10 @@ import com.photo.server.starsnap.global.security.jwt.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
+@EnableRedisRepositories
 @EnableConfigurationProperties(JwtProperties::class)
 class StarsnapApplication
 
--- src/main/kotlin/com/photo/server/starsnap/global/config/RedisConfig.kt
+++ src/main/kotlin/com/photo/server/starsnap/global/config/RedisConfig.kt
@@ -3,13 +3,13 @@ package com.photo.server.starsnap.global.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer
-import org.springframework.data.redis.serializer.StringRedisSerializer
+import org.springframework.data.redis.core.StringRedisTemplate
+
 
 @Configuration
 class RedisConfig(
@@ -18,24 +18,44 @@ class RedisConfig(
     @Value("\${spring.data.redis.password}") private val password: String
 ) {
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
+    @Primary
+    fun refreshTokenConnectionFactory(): RedisConnectionFactory {
+        return redisConnectionFactory(0)
+    }
+
+    @Bean
+    fun refreshTokenRedisTemplate(refreshTokenConnectionFactory: RedisConnectionFactory): StringRedisTemplate {
+        return StringRedisTemplate(refreshTokenConnectionFactory)
+    }
+
+    @Bean
+    fun emailVerifyCodeConnectionFactory(): RedisConnectionFactory {
+        return redisConnectionFactory(1)
+    }
+
+    @Bean
+    fun emailVerifyCodeRedisTemplate(emailVerifyCodeConnectionFactory: RedisConnectionFactory): StringRedisTemplate {
+        return StringRedisTemplate(emailVerifyCodeConnectionFactory)
+    }
+
+    @Bean
+    fun emailTokenConnectionFactory(): RedisConnectionFactory {
+        return redisConnectionFactory(2)
+    }
+
+    @Bean
+    fun emailTokenRedisTemplate(emailTokenConnectionFactory: RedisConnectionFactory): StringRedisTemplate {
+        return StringRedisTemplate(emailTokenConnectionFactory)
+    }
+
+    fun redisConnectionFactory(dbIndex: Int): RedisConnectionFactory {
         val redisStandaloneConfiguration = RedisStandaloneConfiguration()
         redisStandaloneConfiguration.hostName = redisHost
         redisStandaloneConfiguration.port = redisPort
         redisStandaloneConfiguration.password = RedisPassword.of(password)
+        redisStandaloneConfiguration.database = dbIndex
 
         val lettuceConnectionFactory = LettuceConnectionFactory(redisStandaloneConfiguration)
         return lettuceConnectionFactory
-    }
-
-    @Bean
-    fun redisTemplate(): RedisTemplate<String, String> {
-        val template = RedisTemplate<String, String>()
-        with(template) {
-            connectionFactory = redisConnectionFactory()
-            keySerializer = StringRedisSerializer()
-            valueSerializer = JdkSerializationRedisSerializer()
-        }
-        return template
     }
 }
