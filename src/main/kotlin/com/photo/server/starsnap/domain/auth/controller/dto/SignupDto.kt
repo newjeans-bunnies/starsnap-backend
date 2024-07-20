da4c8ffa3baa6c2e60e9339692253037688659f2
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-07-21 00:55:57 +0900

    :art: :: set dto

--- src/main/kotlin/com/photo/server/starsnap/domain/auth/controller/dto/SignupDto.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/auth/controller/dto/SignupDto.kt
@@ -8,9 +8,17 @@ data class SignupDto(
     @field:Pattern(regexp = "^[a-zA-Z0-9]{4,12}$", message = "닉네임은 4~12자 영문 대 소문자, 숫자만 사용하세요.")
     val username: String,
     @field:NotBlank(message = "비밀번호는 필수 입력 값입니다.")
-    @field:Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\\d)(?=.*[~․!@#\$%^&*()_\\-+=|\\\\;:‘“<>,.?/]).{8,50}\$", message = "비밀번호는 8~50자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
+    @field:Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\\d)(?=.*[~․!@#\$%^&*()_\\-+=|\\\\;:‘“<>,.?/]).{8,50}\$",
+        message = "비밀번호는 8~50자 영문 대 소문자, 숫자, 특수문자를 사용하세요."
+    )
     val password: String,
-    @field:NotBlank(message = "전화번호는 필수 입력 값입니다.")
-    @field:Pattern(regexp = "^(010)([0-9]{4})([0-9]{4})$", message = "전화번호를 적어주세요 ex) 01012345678")
-    val phone: String
+    @field:NotBlank(message = "이메일은 필수 입력 값입니다.")
+    @field:Pattern(
+        regexp = "^[a-zA-Z0-9+-_.]+@[0-9a-zA-Z]+\\.[a-zA-Z]{2,3}\$",
+        message = "이메일을 적어주세요 ex) test@test.com"
+    )
+    val email: String,
+    @field:NotBlank(message = "token은 필수 입력 값입니다.")
+    val token: String
 )
