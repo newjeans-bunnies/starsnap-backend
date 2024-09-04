e07e1a210dffa6c6dd3c459f21f43548939a9de0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-04 11:59:47 +0900

    :fire: :: remove dto, .gitkeep file

--- src/main/kotlin/com/photo/server/starsnap/domain/photo/dto/PhotoData.kt
+++
@@ -1,8 +0,0 @@
-package com.photo.server.starsnap.domain.photo.dto
-
-data class PhotoData(
-    val name: String,
-    val createdAt: String,
-    val createdBy: String,
-    val imageUrl: List<String>
-)
