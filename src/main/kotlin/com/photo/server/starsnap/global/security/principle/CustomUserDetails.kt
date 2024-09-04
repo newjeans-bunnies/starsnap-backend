4f9a4743f8c9aa467e685805bc6c6f3aab6b4d88
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-04 12:00:53 +0900

    :zap: :: Add :: ability to retrieve user entity from CustomUserDetails

--- src/main/kotlin/com/photo/server/starsnap/global/security/principle/CustomUserDetails.kt
+++ src/main/kotlin/com/photo/server/starsnap/global/security/principle/CustomUserDetails.kt
@@ -1,13 +1,14 @@
 package com.photo.server.starsnap.global.security.principle
 
 import com.photo.server.starsnap.domain.auth.type.Authority
+import com.photo.server.starsnap.domain.user.UserEntity
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
 
 class CustomUserDetails(
-    private val userId: String, private val authority: Authority
-) : UserDetails {
+    private val userId: String, private val authority: Authority, private val userData: UserEntity
+) : BaseCustomUserDetails {
 
     override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
         return mutableListOf(SimpleGrantedAuthority(authority.name))
@@ -24,4 +25,11 @@ class CustomUserDetails(
     override fun isCredentialsNonExpired(): Boolean = true
 
     override fun isEnabled(): Boolean = true
+
+    override fun getUserData(): UserEntity = userData
+}
+
+interface BaseCustomUserDetails: UserDetails {
+    fun getUserData(): UserEntity
+
 }
