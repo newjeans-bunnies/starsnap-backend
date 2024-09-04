7b790c85df2c4478caa3108f068ec147f977b427
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-04 14:18:48 +0900

    :fire: :: remove image

--- src/main/kotlin/com/photo/server/starsnap/domain/snap/repository/ImageRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.photo.server.starsnap.domain.snap.repository
-
-import com.photo.server.starsnap.domain.snap.ImageEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ImageRepository: JpaRepository<ImageEntity, String>
