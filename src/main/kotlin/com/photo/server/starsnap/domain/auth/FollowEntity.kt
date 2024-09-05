f4d0d67e8a632c3c93345974d60340cf95432ade
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-05 15:05:35 +0900

    :zap: :: add node name

--- src/main/kotlin/com/photo/server/starsnap/domain/auth/FollowEntity.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/auth/FollowEntity.kt
@@ -5,10 +5,10 @@ import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
 import org.springframework.data.neo4j.core.schema.Relationship
 
-@Node
+@Node("follow")
 data class FollowEntity(
     @Id
     val userId: String,
-    @Relationship
-    val follow: List<FollowEntity> = emptyList()
+    @Relationship(type = "FRIEND_OF")
+    var follow: List<FollowEntity> = emptyList()
 )
