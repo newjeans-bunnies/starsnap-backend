168af1c71dc96924b8e1f32ffabeaf48c3110ad3
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-16 22:11:34 +0900

    :fire: :: remove neo4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-data-neo4j")
-    implementation("org.neo4j.driver:neo4j-java-driver:5.23.0")
 
     implementation("ognl:ognl:3.3.4")
 
