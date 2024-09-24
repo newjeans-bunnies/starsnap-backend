3acdbb62aac1d9853141c945b050a9d14a468b00
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-24 16:59:36 +0900

    :art: :: change user change url

---
+++ src/main/kotlin/com/photo/server/starsnap/domain/user/controller/ChangeUserDataController.kt
@@ -0,0 +1,45 @@
+package com.photo.server.starsnap.domain.user.controller
+
+import com.photo.server.starsnap.domain.user.service.UserAwsS3Service
+import com.photo.server.starsnap.domain.user.service.UserService
+import com.photo.server.starsnap.global.dto.StatusDto
+import com.photo.server.starsnap.global.security.principle.CustomUserDetails
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@Validated
+@RestController
+@RequestMapping("/api/user/change-data")
+class ChangeUserDataController(
+    private val userService: UserService,
+    private val userAwsS3Service: UserAwsS3Service
+) {
+
+    @PatchMapping("/username")
+    fun changeUsername(
+        @NotBlank(message = "닉네임은 필수 입력 값입니다.") @Pattern(
+            regexp = "^[a-zA-Z0-9]{4,12}$", message = "닉네임은 4~12자 영문 대 소문자, 숫자만 사용하세요."
+        ) @Valid @RequestParam username: String, @AuthenticationPrincipal auth: CustomUserDetails
+    ): StatusDto {
+        userService.changeUsername(username, auth.username)
+        return StatusDto("OK", 200)
+    }
+
+    @PatchMapping("/profile-image")
+    fun changeProfileImage(
+        @AuthenticationPrincipal user: CustomUserDetails,
+        @Valid @RequestParam image: MultipartFile
+    ): StatusDto {
+        userAwsS3Service.changeProfileImage(user.username, image)
+        return StatusDto("OK", 200)
+    }
+
+}
--- src/main/kotlin/com/photo/server/starsnap/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/user/controller/UserController.kt
@@ -1,26 +1,16 @@
 package com.photo.server.starsnap.domain.user.controller
 
-import com.photo.server.starsnap.domain.user.service.UserAwsS3Service
+import com.photo.server.starsnap.domain.user.controller.dto.UserDto
 import com.photo.server.starsnap.domain.user.service.UserService
-import com.photo.server.starsnap.global.dto.StatusDto
-import com.photo.server.starsnap.global.security.principle.CustomUserDetails
-import jakarta.validation.Valid
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Pattern
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
 
-@Validated
-@RestController
 @RequestMapping("/api/user")
+@RestController
 class UserController(
-    private val userService: UserService,
-    private val userAwsS3Service: UserAwsS3Service
+    private val userService: UserService
 ) {
     @GetMapping
     fun getUserData(@RequestParam("user-id") userId: String): UserDto {
