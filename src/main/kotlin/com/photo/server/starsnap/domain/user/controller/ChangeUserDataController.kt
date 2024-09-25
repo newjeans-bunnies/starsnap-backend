87a6542bc3ee1f4a818a2b791faa86029d97d98a
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-09-25 14:14:41 +0900

    :zap: :: change user api response

--- src/main/kotlin/com/photo/server/starsnap/domain/user/controller/ChangeUserDataController.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/user/controller/ChangeUserDataController.kt
@@ -3,16 +3,15 @@ package com.photo.server.starsnap.domain.user.controller
 import com.photo.server.starsnap.domain.user.service.UserAwsS3Service
 import com.photo.server.starsnap.domain.user.service.UserService
 import com.photo.server.starsnap.global.dto.StatusDto
+import com.photo.server.starsnap.global.dto.UserDto
 import com.photo.server.starsnap.global.security.principle.CustomUserDetails
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
+import org.springframework.http.HttpStatus
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @Validated
@@ -23,16 +22,18 @@ class ChangeUserDataController(
     private val userAwsS3Service: UserAwsS3Service
 ) {
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/username")
     fun changeUsername(
         @NotBlank(message = "닉네임은 필수 입력 값입니다.") @Pattern(
             regexp = "^[a-zA-Z0-9]{4,12}$", message = "닉네임은 4~12자 영문 대 소문자, 숫자만 사용하세요."
         ) @Valid @RequestParam username: String, @AuthenticationPrincipal auth: CustomUserDetails
-    ): StatusDto {
-        userService.changeUsername(username, auth.username)
-        return StatusDto("OK", 200)
+    ): UserDto {
+        val userData = userService.changeUsername(username, auth.username)
+        return userData
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/profile-image")
     fun changeProfileImage(
         @AuthenticationPrincipal user: CustomUserDetails,
