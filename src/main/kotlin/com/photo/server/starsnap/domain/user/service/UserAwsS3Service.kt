6aaea19b32fe3dfa0ac70b57d01770f9895150cd
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-10-17 15:54:45 +0900

    :zap: :: add upload oauth profile image

--- src/main/kotlin/com/photo/server/starsnap/domain/user/service/UserAwsS3Service.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/user/service/UserAwsS3Service.kt
@@ -7,10 +7,16 @@ import com.photo.server.starsnap.global.config.AwsS3Config
 import com.photo.server.starsnap.global.error.exception.NotExistUserIdException
 import com.photo.server.starsnap.global.utils.type.isValid
 import com.photo.server.starsnap.global.utils.type.toType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
+import java.io.*
+import java.net.URL
+import javax.imageio.ImageIO
 
 @Service
 class UserAwsS3Service(
@@ -19,6 +25,7 @@ class UserAwsS3Service(
     @Value("\${cloud.aws.s3.bucket}")
     private val bucket: String
 ) {
+
     fun changeProfileImage(userId: String, profileImage: MultipartFile) {
 
         val userData = userRepository.findByIdOrNull(userId) ?: throw NotExistUserIdException
@@ -32,15 +39,67 @@ class UserAwsS3Service(
 
         val putObjectRequest = PutObjectRequest(
             bucket,
-            "profile/$userId",
+            "profile/$userId.jpg",
             profileImage.inputStream,
             objectMetadata,
         )
 
         awsS3Config.amazonS3Client().putObject(putObjectRequest)
 
-        userData.profileImageUrl = "profile/$userId"
+        userData.profileImageUrl = "profile/$userId.jpg"
 
         userRepository.save(userData)
+    }
+
+    fun addOauthProfileImage(userId: String, profileImageURL: String) = CoroutineScope(Dispatchers.IO).launch {
+        val imageUrl = URL(profileImageURL)
+        val inputSteam = imageUrl.openStream()
+        val bos = ByteArrayOutputStream()
+
+        val image = ImageIO.read(inputSteam)
+        ImageIO.write(image, "jpg", bos)
+
+
+        val multipartFile = CustomMultipartFile(bos.toByteArray(), profileImageURL)
+        awsS3Config.amazonS3Client().putObject(createObject(multipartFile, userId))
+    }
+
+    private fun createObject(imageData: MultipartFile, userId: String): PutObjectRequest {
+        val objectMetadata = ObjectMetadata().apply {
+            this.contentType = imageData.contentType
+            this.contentLength = imageData.size
+        }
+
+        val putObjectRequest = PutObjectRequest(
+            bucket,
+            "profile/$userId.jpg",
+            imageData.inputStream,
+            objectMetadata,
+        )
+
+        return putObjectRequest
+    }
+}
+
+
+class CustomMultipartFile(private val input: ByteArray, private val filename: String) : MultipartFile {
+    override fun getInputStream(): InputStream = ByteArrayInputStream(input)
+
+    override fun getName(): String = ""
+
+    override fun getOriginalFilename(): String = filename
+
+    override fun getContentType(): String? = null
+
+    override fun isEmpty(): Boolean = input.isEmpty();
+
+    override fun getSize(): Long = input.size.toLong()
+
+    override fun getBytes(): ByteArray = input
+
+    override fun transferTo(dest: File) {
+        FileOutputStream(dest).use { fos ->
+            fos.write(input)
+        }
     }
 }
