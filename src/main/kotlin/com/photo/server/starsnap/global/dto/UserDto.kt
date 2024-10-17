a9664ff4fb1e19a5c2e4789371ee079b1f585627
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-10-17 16:04:20 +0900

    :zap: :: commit

--- src/main/kotlin/com/photo/server/starsnap/global/dto/UserDto.kt
+++ src/main/kotlin/com/photo/server/starsnap/global/dto/UserDto.kt
@@ -1,14 +1,17 @@
 package com.photo.server.starsnap.global.dto
 
+import com.photo.server.starsnap.domain.auth.type.Oauth2
 import com.photo.server.starsnap.domain.user.entity.UserEntity
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 
 data class UserDto(
     val userId: String,
     val username: String,
+    val nickname: String,
     val email: String,
     val profileImageUrl: String? = null,
     val authority: String,
-    val followCount: Int,
+    val followingCount: Int,
     val followerCount: Int
 )
 
@@ -19,5 +22,13 @@ fun UserEntity.toUserDto() = UserDto(
     profileImageUrl = profileImageUrl,
     authority = authority.name,
     followerCount = followerCount,
-    followCount = followingCount
+    followingCount = followingCount,
+    nickname = nickname
 )
+
+fun OAuth2UserRequest.of(registrationId: String, attributes: Map<String, Any>): OAuthDto {
+    return when (registrationId) {
+        "google" -> OAuthDto(Oauth2.GOOGLE, attributes["email"].toString(), attributes["name"].toString(), attributes["picture"].toString(), attributes["sub"].toString(), attributes["name"].toString())
+        else -> throw RuntimeException("Unsupported OAuth2 user request")
+    }
+}
