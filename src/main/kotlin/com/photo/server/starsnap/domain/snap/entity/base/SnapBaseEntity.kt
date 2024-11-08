79fc1f27fc4f6145e5e228d1f4ffaebf5fad4f9d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-11-08 23:44:20 +0900

    :zap: :: fix time value string -> LocalDateTime

---
+++ src/main/kotlin/com/photo/server/starsnap/domain/snap/entity/base/SnapBaseEntity.kt
@@ -0,0 +1,25 @@
+package com.photo.server.starsnap.domain.snap.entity.base
+
+import jakarta.persistence.*
+import io.viascom.nanoid.NanoId
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(value = [EntityListeners::class])
+abstract class SnapBaseEntity {
+    @Id
+    open var id = NanoId.generate(16)
+        protected set
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    var createdAt: LocalDateTime? = null
+        protected set
+
+    @LastModifiedDate
+    @Column(name = "modified_at", columnDefinition = "DATETIME")
+    open var modifiedAt: LocalDateTime? = null
+        protected set
+}
