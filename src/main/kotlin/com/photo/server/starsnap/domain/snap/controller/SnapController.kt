cf70bfff5176bb36e9cb6f102d07132767619bb0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-11-08 23:43:29 +0900

    :zap: :: add find by snap with tag api

--- src/main/kotlin/com/photo/server/starsnap/domain/snap/controller/SnapController.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/snap/controller/SnapController.kt
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Slice
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/snap")
@@ -28,7 +29,8 @@ class SnapController(
         @RequestPart("image") image: MultipartFile, // 사진
         @RequestPart("title") title: String, // 글
         @RequestPart("source") source: String, // 사진 출처
-        @RequestPart("date-taken") dateTaken: String // 사진 찍은 날짜
+        @RequestPart("date-taken") dateTaken: LocalDateTime, // 사진 찍은 날짜
+        @RequestPart("tag") tags: List<String>,
     ): StatusDto {
         if(!bucketConfig.createSnapBucket().tryConsume(1)) throw TooManyRequestException
 
@@ -37,16 +39,17 @@ class SnapController(
             title = title,
             image = image,
             source = source,
-            dateTaken = dateTaken
+            dateTaken = dateTaken,
+            tags = tags
         )
 
         return StatusDto("created snap", 201)
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/get")
-    fun getSnap(@RequestParam page: Int, @RequestParam size: Int): Slice<SnapResponseDto> {
-        val snapData = snapService.getSnap(size, page)
+    @GetMapping
+    fun getSnap(@RequestParam page: Int, @RequestParam size: Int, @RequestParam(required = false, defaultValue = "") tag: String): Slice<SnapResponseDto> {
+        val snapData = snapService.getSnap(size, page, tag)
         return snapData
     }
 
@@ -56,7 +59,7 @@ class SnapController(
         @RequestPart("snap-id") snapId: String,
         @RequestPart("source") source: String,
         @RequestPart("title") title: String,
-        @RequestPart("date-taken") dateTaken: String,
+        @RequestPart("date-taken") dateTaken: LocalDateTime,
         @RequestPart(name = "image", required = false) image: MultipartFile?,
         @AuthenticationPrincipal user: CustomUserDetails
     ): SnapResponseDto {
--- src/main/kotlin/com/photo/server/starsnap/domain/snap/repository/SnapRepository.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/snap/repository/SnapRepository.kt
@@ -1,6 +1,6 @@
 package com.photo.server.starsnap.domain.snap.repository
 
-import com.photo.server.starsnap.domain.snap.SnapEntity
+import com.photo.server.starsnap.domain.snap.entity.SnapEntity
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.jpa.repository.JpaRepository
@@ -9,4 +9,7 @@ import org.springframework.data.jpa.repository.Query
 interface SnapRepository : JpaRepository<SnapEntity, String> {
     @Query("SELECT snap FROM SnapEntity snap")
     fun findSliceBy(pageable: Pageable): Slice<SnapEntity>?
+
+    @Query("SELECT snap FROM SnapEntity snap JOIN snap.tags tag WHERE tag.name IN :tag")
+    fun findSnapTag(pageable: Pageable, tag: String): Slice<SnapEntity>?
 }
