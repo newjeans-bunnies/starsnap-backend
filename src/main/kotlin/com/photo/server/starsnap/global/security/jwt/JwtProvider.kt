26e979ba830f35ea93e2ce736e206cf2c67407a7
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-11-14 23:01:36 +0900

    :zap: :: add register token

--- src/main/kotlin/com/photo/server/starsnap/global/security/jwt/JwtProvider.kt
+++ src/main/kotlin/com/photo/server/starsnap/global/security/jwt/JwtProvider.kt
@@ -18,11 +18,14 @@ class JwtProvider(
     companion object {
         const val ACCESS = "access"
         const val REFRESH = "refresh"
+        const val REGISTER = "register"
         const val AUTHORITY = "authority"
     }
 
     private val accessKey: Key = Keys.hmacShaKeyFor(jwtProperties.accessSecretKey.toByteArray(StandardCharsets.UTF_8))
     private val refreshKey: Key = Keys.hmacShaKeyFor(jwtProperties.refreshSecretKey.toByteArray(StandardCharsets.UTF_8))
+    private val registerKey: Key =
+        Keys.hmacShaKeyFor(jwtProperties.registerSecretKey.toByteArray(StandardCharsets.UTF_8))
 
     fun receiveToken(id: String, authority: Authority) = TokenDto(
         accessToken = generateJwtAccessToken(id, authority),
@@ -30,6 +33,13 @@ class JwtProvider(
         refreshToken = generateJwtRefreshToken(authority),
         authority = authority.name
     )
+
+    fun registerToken(email: String, sub: String): String = generateJwtRegisterToken(email, sub)
+
+    private fun generateJwtRegisterToken(email: String, sub: String) =
+        Jwts.builder().signWith(registerKey, SignatureAlgorithm.HS256).setHeaderParam(Header.JWT_TYPE, REGISTER).setId(sub)
+            .claim("email", email).setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + jwtProperties.registerExp * 1000)).compact()
 
     private fun generateJwtAccessToken(uuid: String, authority: Authority) =
         Jwts.builder().signWith(accessKey, SignatureAlgorithm.HS256).setHeaderParam(Header.JWT_TYPE, ACCESS).setId(uuid)
