2a1eb5795b474ba548ab8c893058223fda592ae8
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-11-26 22:17:50 +0900

    :zap: :: add api to find star-group

--- src/main/kotlin/com/photo/server/starsnap/domain/star/controller/StarGroupController.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/star/controller/StarGroupController.kt
@@ -1,39 +1,56 @@
 package com.photo.server.starsnap.domain.star.controller
 
 import com.photo.server.starsnap.domain.auth.type.Authority
-import com.photo.server.starsnap.domain.star.dto.CreateStarGroupDto
+import com.photo.server.starsnap.domain.star.dto.CreateStarGroupRequestDto
 import com.photo.server.starsnap.domain.star.dto.ExistDto
-import com.photo.server.starsnap.domain.star.dto.UpdateStarGroupDto
+import com.photo.server.starsnap.domain.star.dto.StarGroupResponseDto
+import com.photo.server.starsnap.domain.star.dto.UpdateStarGroupRequestDto
 import com.photo.server.starsnap.domain.star.service.StarGroupService
 import com.photo.server.starsnap.global.security.principle.CustomUserDetails
 import jakarta.validation.Valid
+import org.springframework.data.domain.Slice
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/star-group")
 class StarGroupController(
     private val starGroupService: StarGroupService
 ) {
-    @PostMapping
+    @GetMapping("get")
+    fun getStarGroups(
+        @AuthenticationPrincipal user: CustomUserDetails,
+        @RequestParam page: Int, @RequestParam size: Int
+    ): Slice<StarGroupResponseDto> {
+        if (user.authority != Authority.ADMIN) throw RuntimeException("권한 없음")
+        return starGroupService.getStarGroup(size, page)
+    }
+
+    @PostMapping("/create")
     fun createStarGroup(
-        @Valid @RequestBody starGroupDto: CreateStarGroupDto,
+        @Valid @RequestBody starGroupDto: CreateStarGroupRequestDto,
         @AuthenticationPrincipal user: CustomUserDetails
-    ) {
-        if(user.authority != Authority.ADMIN) throw RuntimeException("권한 없음")
-        starGroupService.createStarGroup(starGroupDto)
+    ): StarGroupResponseDto {
+        if (user.authority != Authority.ADMIN) throw RuntimeException("권한 없음")
+        return starGroupService.createStarGroup(starGroupDto)
     }
 
-    @PatchMapping
+    @PatchMapping("/update")
     fun updateStarGroup(
-        @Valid @RequestBody starGroupDto: UpdateStarGroupDto,
+        @Valid @RequestBody starGroupDto: UpdateStarGroupRequestDto,
         @AuthenticationPrincipal user: CustomUserDetails
-    ) {
-        if(user.authority != Authority.ADMIN) throw RuntimeException("권한 없음")
-        starGroupService.updateStarGroup(starGroupDto)
+    ): StarGroupResponseDto {
+        if (user.authority != Authority.ADMIN) throw RuntimeException("권한 없음")
+        return starGroupService.updateStarGroup(starGroupDto)
     }
 
-    @GetMapping
-    fun existStarGroups(@RequestParam type: String, @RequestParam name: String): ExistDto {
-        return starGroupService.existStarGroup(type, name )
+    @GetMapping("/exist")
+    fun existStarGroups(
+        @RequestParam type: String,
+        @RequestParam name: String,
+        @AuthenticationPrincipal user: CustomUserDetails
+    ): ExistDto {
+        if (user.authority != Authority.ADMIN) throw RuntimeException("권한 없음")
+        return starGroupService.existStarGroup(type, name)
     }
 }
