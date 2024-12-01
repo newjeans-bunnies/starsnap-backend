9bd4a9fe51a2f99e7ce7d85529653da83db02e25
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-01 20:54:45 +0900

    :zap: :: fix scheduling

--- src/main/kotlin/com/photo/server/starsnap/global/config/SchedulingConfig.kt
+++ src/main/kotlin/com/photo/server/starsnap/global/config/SchedulingConfig.kt
@@ -3,6 +3,8 @@ package com.photo.server.starsnap.global.config
 import com.photo.server.starsnap.domain.snap.repository.SnapRepository
 import com.photo.server.starsnap.domain.user.repository.UserRepository
 import jakarta.transaction.Transactional
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -15,9 +17,11 @@ class SchedulingConfig(
     private val userRepository: UserRepository,
     private val snapRepository: SnapRepository,
 ) {
+    var logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     @Scheduled(cron = "0 0 12 * * *")
-    @Async
     @Transactional
+    @Async
     fun secessionUser() {
         val sevenDaysAgo = LocalDateTime.now().minusDays(7) // 7일 전 날짜 계산
         userRepository.deleteOldUser(sevenDaysAgo)
