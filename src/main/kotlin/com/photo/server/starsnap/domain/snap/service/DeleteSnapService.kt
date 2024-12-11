48e03012fb0a3f145db5f7fae5e910c8dc2267a1
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2024-12-11 22:55:54 +0900

    :zap: :: add Custom Exception

--- src/main/kotlin/com/photo/server/starsnap/domain/snap/service/DeleteSnapService.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/snap/service/DeleteSnapService.kt
@@ -1,9 +1,12 @@
 package com.photo.server.starsnap.domain.snap.service
 
+import com.photo.server.starsnap.domain.snap.error.exception.NotFoundSnapIdException
 import com.photo.server.starsnap.domain.snap.repository.SnapRepository
+import com.photo.server.starsnap.domain.user.error.exception.NotFoundUserIdException
 import com.photo.server.starsnap.domain.user.repository.UserRepository
 import com.photo.server.starsnap.global.dto.SnapDto
 import com.photo.server.starsnap.global.dto.toSnapDto
+import com.photo.server.starsnap.global.error.exception.InvalidRoleException
 import jakarta.transaction.Transactional
 import org.springframework.data.domain.Slice
 import org.springframework.data.repository.findByIdOrNull
@@ -16,7 +19,7 @@ class DeleteSnapService(
 ) {
     @Transactional
     fun getDeleteSnap(page: Int, size: Int, userId: String): Slice<SnapDto> {
-        val snaps = snapRepository.findSliceByStateAndUserId(false, userId) ?: throw RuntimeException("존재 하지 않는 Snap")
+        val snaps = snapRepository.findSliceByStateAndUserId(false, userId) ?: throw NotFoundSnapIdException
         return snaps.map {
             it.toSnapDto()
         }
@@ -24,9 +27,9 @@ class DeleteSnapService(
 
     @Transactional
     fun rollbackSnap(snapId: String, userId: String): SnapDto {
-        val snap = snapRepository.findByIdOrNull(snapId) ?: throw RuntimeException("존재 하지 않는 SnapId")
-        val user = userRepository.findByIdOrNull(userId) ?: throw RuntimeException("존재 하지 않는 UserId")
-        if (user != snap.user) throw RuntimeException("권한 없음")
+        val snap = snapRepository.findByIdOrNull(snapId) ?: throw NotFoundSnapIdException
+        val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundUserIdException
+        if (user != snap.user) throw InvalidRoleException
         snap.state = true
 
         snapRepository.save(snap)
--- src/main/kotlin/com/photo/server/starsnap/domain/snap/service/LikeService.kt
+++ src/main/kotlin/com/photo/server/starsnap/domain/snap/service/LikeService.kt
@@ -1,6 +1,7 @@
 package com.photo.server.starsnap.domain.snap.service
 
 import com.photo.server.starsnap.domain.snap.entity.LikeEntity
+import com.photo.server.starsnap.domain.snap.error.exception.NotFoundSnapIdException
 import com.photo.server.starsnap.domain.snap.repository.LikeRepository
 import com.photo.server.starsnap.domain.snap.repository.SnapRepository
 import com.photo.server.starsnap.domain.user.entity.UserEntity
@@ -14,14 +15,14 @@ class LikeService(
     private val snapRepository: SnapRepository
 ) {
     fun likeSnap(user: UserEntity, snapId: String): StatusDto {
-        val snap = snapRepository.findByIdOrNull(snapId) ?: throw RuntimeException("존재 하지 않는 snapId")
+        val snap = snapRepository.findByIdOrNull(snapId) ?: throw NotFoundSnapIdException
         val like = LikeEntity(user, snap)
         likeRepository.save(like)
         return StatusDto("OK", 200)
     }
 
     fun unlikeSnap(user: UserEntity, snapId: String): StatusDto {
-        val snap = snapRepository.findByIdOrNull(snapId) ?: throw RuntimeException("존재 하지 않는 snapId")
+        val snap = snapRepository.findByIdOrNull(snapId) ?: throw NotFoundSnapIdException
         likeRepository.deleteBySnapAndUser(snap, user)
         return StatusDto("OK", 200)
     }
