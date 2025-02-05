81ecf41a44a5dbc2570593ba6a46a3da201e30bf
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-02-05 21:53:17 +0900

    :zap: :: add valid(email, username) api

---
+++ src/main/kotlin/com/photo/server/starsnap/domain/auth/controller/ValidController.kt
@@ -0,0 +1,24 @@
+package com.photo.server.starsnap.domain.auth.controller
+
+import com.photo.server.starsnap.domain.auth.service.ValidService
+import com.photo.server.starsnap.global.dto.StatusDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/auth/valid")
+class ValidController(
+    private val validService: ValidService
+) {
+    @GetMapping("/username")
+    fun validUsername(@RequestParam("username") username: String): StatusDto {
+        return validService.validUsername(username)
+    }
+
+    @GetMapping("/email")
+    fun validEmail(@RequestParam("email") email: String): StatusDto {
+        return validService.validEmail(email)
+    }
+}
