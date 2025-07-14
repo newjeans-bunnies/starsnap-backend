b9676f43bf19437c740cc35e9e5a6c2128209ef0
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-07-14 23:44:40 +0900

    :zap: :: remove unused UserEntity import from FanCrudRepository for cleaner code

--- adapter-infrastructure/src/main/kotlin/com/photo/server/starsnap/adapter_infrastructure/star/repository/springdata/FanCrudRepository.kt
+++ adapter-infrastructure/src/main/kotlin/com/photo/server/starsnap/adapter_infrastructure/star/repository/springdata/FanCrudRepository.kt
@@ -3,7 +3,6 @@ package com.photo.server.starsnap.adapter_infrastructure.star.repository.springd
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 import com.photo.server.starsnap.adapter_infrastructure.star.entity.FanEntity
-import com.photo.server.starsnap.adapter_infrastructure.user.entity.UserEntity
 
 @Repository
 interface FanCrudRepository: CrudRepository<FanEntity, String> {
