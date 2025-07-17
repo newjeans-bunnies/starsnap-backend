76f7115881c5047e83ade9813fc405bd34ea879f
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-07-18 00:28:19 +0900

    :zap: :: set AwsSQSConfig

---
+++ adapter-lambda-s3/src/main/kotlin/com/photo/server/starsnap/adapter_lambda_s3/config/AwsSQSConfig.kt
@@ -0,0 +1,38 @@
+package com.photo.server.starsnap.adapter_lambda_s3.config
+
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory
+import io.awspring.cloud.sqs.operations.SqsTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+
+@Configuration
+open class AwsSQSConfig(
+    @Value("\${cloud.aws.region.static}") private val region: String,
+    @Value("\${cloud.aws.credentials.access-key}") private val accessKey: String,
+    @Value("\${cloud.aws.credentials.secret-key}") private val secretKey: String,
+) {
+    @Bean
+    open fun sqsAsyncClient(): SqsAsyncClient {
+        return SqsAsyncClient.builder()
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+            .build()
+    }
+
+    @Bean
+    open fun sqsTemplate(): SqsTemplate {
+        return SqsTemplate.newTemplate(sqsAsyncClient())
+    }
+
+    @Bean
+    open fun defaultSqsListenerContainerFactory(): SqsMessageListenerContainerFactory<Object> {
+        return SqsMessageListenerContainerFactory.builder<Object>()
+            .sqsAsyncClient(sqsAsyncClient())
+            .build()
+    }
+}
