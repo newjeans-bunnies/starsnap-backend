ed7110b553d19dfd32f743bcaced2b9cc6abedd2
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-07-22 23:08:01 +0900

    :heavy_plus_sign: :: add SQS message receiver and sender services for photo and video processing

---
+++ config/src/main/kotlin/com/photo/server/starsnap/config/AwsSQSConfig.kt
@@ -0,0 +1,47 @@
+package com.photo.server.starsnap.config
+
+import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory
+import io.awspring.cloud.sqs.operations.SqsTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.sqs.SqsAsyncClient
+import software.amazon.awssdk.services.sqs.SqsClient
+
+@Configuration
+open class AwsSQSConfig(
+    @Value("\${spring.cloud.aws.region.static}") private val region: String,
+    @Value("\${spring.cloud.aws.credentials.access-key}") private val accessKey: String,
+    @Value("\${spring.cloud.aws.credentials.secret-key}") private val secretKey: String,
+) {
+    @Bean
+    open fun sqsAsyncClient(): SqsAsyncClient {
+        return SqsAsyncClient.builder()
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+            .build()
+    }
+
+    @Bean
+    open fun sqsClient(): SqsClient {
+        return SqsClient.builder()
+            .region(Region.of(region))
+            .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+            .build()
+    }
+
+    @Bean
+    open fun sqsTemplate(): SqsTemplate {
+        return SqsTemplate.newTemplate(sqsAsyncClient())
+    }
+
+    @Bean
+    open fun defaultSqsListenerContainerFactory(): SqsMessageListenerContainerFactory<Any> {
+        return SqsMessageListenerContainerFactory.builder<Any>()
+            .sqsAsyncClient(sqsAsyncClient())
+            .build()
+    }
+}
