59125ded04d89cedb7c16b9c2d5acd6eac71895e
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-07-22 23:09:11 +0900

    :construction: :: enhance AwsUseCase to include user information in presigned URL creation and save photo/video entities

--- adapter-usecase/src/main/kotlin/com/photo/server/starsnap/adapter_usecase/file/usecase/AwsUseCaseImpl.kt
+++ adapter-usecase/src/main/kotlin/com/photo/server/starsnap/adapter_usecase/file/usecase/AwsUseCaseImpl.kt
@@ -1,6 +1,12 @@
 package com.photo.server.starsnap.adapter_usecase.file.usecase
 
+import com.photo.server.starsnap.adapter_infrastructure.file.repository.PhotoRepositoryImpl
+import com.photo.server.starsnap.adapter_infrastructure.file.repository.VideoRepositoryImpl
 import com.photo.server.starsnap.config.AwsConfig
+import com.photo.server.starsnap.domain.file.entity.Photo
+import com.photo.server.starsnap.domain.file.entity.Video
+import com.photo.server.starsnap.domain.file.type.Status
+import com.photo.server.starsnap.domain.user.entity.User
 import com.photo.server.starsnap.usecase.file.dto.UploadFileRequest
 import com.photo.server.starsnap.usecase.file.usecase.AwsUseCase
 import org.springframework.beans.factory.annotation.Value
@@ -8,6 +14,7 @@ import org.springframework.stereotype.Service
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest
+import java.time.LocalDateTime
 
 @Service
 class AwsUseCaseImpl(
@@ -15,20 +22,26 @@ class AwsUseCaseImpl(
     @Value("\${spring.cloud.aws.region.static}")
     private val region: Region,
     @Value("\${spring.cloud.aws.s3.input-bucket}")
-    private val bucket: String
-): AwsUseCase {
+    private val bucket: String,
+
+    private val photoRepositoryImpl: PhotoRepositoryImpl,
+    private val videoRepositoryImpl: VideoRepositoryImpl
+) : AwsUseCase {
 
     // Presigned URL 생성
-     override fun createPresignedUploadUrl(key: String, metadata: UploadFileRequest): String {
+    override fun createPresignedUploadUrl(key: String, metadata: UploadFileRequest, user: User): String {
         val presigner = awsConfig.amazonS3Presigner()
 
         val putObjectRequest = PutObjectRequest.builder()
             .bucket(bucket)
             .key(key)
-            .metadata(mapOf(
-                "ai-state" to metadata.aiState.toString(),
-                "date-taken" to metadata.dateTaken.toString(),
-                "source" to metadata.source)
+            .metadata(
+                mapOf(
+                    "ai-state" to metadata.aiState.toString(),
+                    "date-taken" to metadata.dateTaken,
+                    "source" to metadata.source,
+                    "user-id" to user.id
+                )
             )
             .build()
 
@@ -38,6 +51,47 @@ class AwsUseCaseImpl(
             .build()
 
         val presignedRequest = presigner.presignPutObject(presignRequest)
+        when{
+            key.startsWith("photo/") -> {
+                val photo = Photo(
+                    fileKey = key,
+                    source = metadata.source,
+                    aiState = metadata.aiState,
+                    dateTaken = parseLocalDateTimeOrNull(metadata.dateTaken),
+                    user = user,
+                    createdAt = LocalDateTime.now(),
+                    status = Status.INIT
+                )
+                photoRepositoryImpl.save(photo)
+            }
+
+            // 동영상 처리 로직
+            key.startsWith("video/") -> {
+                val video = Video(
+                    fileKey = key,
+                    source = metadata.source,
+                    aiState = metadata.aiState,
+                    dateTaken = parseLocalDateTimeOrNull(metadata.dateTaken),
+                    createdAt = LocalDateTime.now(),
+                    user = user,
+                    status = Status.INIT
+                )
+                videoRepositoryImpl.save(video)
+            }
+            else -> throw RuntimeException("Unknown key $key")
+        }
+
+
         return presignedRequest.url().toString()
     }
+
+    private fun parseLocalDateTimeOrNull(input: String?): LocalDateTime? {
+        return try {
+            if (input.isNullOrBlank()) return null
+            LocalDateTime.parse(input)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
 }
--- usecase/src/main/kotlin/com/photo/server/starsnap/usecase/file/usecase/AwsUseCase.kt
+++ usecase/src/main/kotlin/com/photo/server/starsnap/usecase/file/usecase/AwsUseCase.kt
@@ -1,7 +1,8 @@
 package com.photo.server.starsnap.usecase.file.usecase
 
+import com.photo.server.starsnap.domain.user.entity.User
 import com.photo.server.starsnap.usecase.file.dto.UploadFileRequest
 
 interface AwsUseCase {
-    fun createPresignedUploadUrl(key: String, metadata: UploadFileRequest): String
+    fun createPresignedUploadUrl(key: String, metadata: UploadFileRequest, user: User): String
 }
