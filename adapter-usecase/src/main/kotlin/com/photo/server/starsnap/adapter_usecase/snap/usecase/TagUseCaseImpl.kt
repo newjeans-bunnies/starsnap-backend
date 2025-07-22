bfd1725eae13209161b2b02fdcffc738af755ef8
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-07-22 23:20:42 +0900

    :heavy_plus_sign: :: update getTag method to create a new tag if not found, improving tag management

--- adapter-usecase/src/main/kotlin/com/photo/server/starsnap/adapter_usecase/snap/usecase/TagUseCaseImpl.kt
+++ adapter-usecase/src/main/kotlin/com/photo/server/starsnap/adapter_usecase/snap/usecase/TagUseCaseImpl.kt
@@ -2,7 +2,6 @@ package com.photo.server.starsnap.adapter_usecase.snap.usecase
 
 import com.photo.server.starsnap.adapter_infrastructure.snap.repository.TagRepositoryImpl
 import com.photo.server.starsnap.domain.snap.entity.Tag
-import com.photo.server.starsnap.exception.snap.error.exception.NotFoundTagException
 import com.photo.server.starsnap.usecase.snap.usecase.TagUseCase
 import org.springframework.stereotype.Service
 import io.viascom.nanoid.NanoId
@@ -23,7 +22,7 @@ class TagUseCaseImpl(
 
     // 태그 조회
     override fun getTag(tag: String): Tag {
-        val tag = tagRepositoryImpl.findByName(tag) ?: throw NotFoundTagException
+        val tag = tagRepositoryImpl.findByName(tag) ?: createTag(tag)
         tag.count += 1
         return tagRepositoryImpl.save(tag)
     }
