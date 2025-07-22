7446af41d11a38d4a01af1cba19eafde650e3275
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2025-07-22 23:10:20 +0900

    :zap: refactor PhotoMetaDateDto and VideoMetaDateDto to simplify properties and change types for file size, width, and height

--- domain/src/main/kotlin/com/photo/server/starsnap/domain/file/dto/VideoMetaDateDto.kt
+++ domain/src/main/kotlin/com/photo/server/starsnap/domain/file/dto/VideoMetaDateDto.kt
@@ -2,14 +2,9 @@ package com.photo.server.starsnap.domain.file.dto
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.time.LocalDateTime
 
 data class VideoMetaDateDto @JsonCreator constructor(
     @JsonProperty("fileKey") val fileKey: String,
-    @JsonProperty("aiState") val aiState: Boolean,
-    @JsonProperty("dateTaken") val dateTaken: LocalDateTime?,
-    @JsonProperty("source") val source: String,
-    @JsonProperty("userId") val userId: String,
-    @JsonProperty("fileSize") val fileSize: Long,
+    @JsonProperty("fileSize") val fileSize: String,
     @JsonProperty("contentType") val contentType: String
 )
